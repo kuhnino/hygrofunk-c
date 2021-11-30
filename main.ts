bf4b5eb502241efe238626374baf27dbda2d1585
kuhnino <77860530+kuhnino@users.noreply.github.com>
2021-11-30 14:33:47 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,17 @@
-radio.onReceivedString(function (receivedString) {
-    EID = receivedString.charAt(0)
-    if (EID != SID) {
-        basic.showString(receivedString)
-        radio.sendString(receivedString)
-    }
-})
-let EID = ""
-let SID = ""
 basic.setLedColor(0xff0000)
 DHT11.setPin(DigitalPin.P3)
-SID = "B"
+let SID = "Sauna"
 let Ti = "Ti"
 let Te = "Te"
 let Le = "Le"
 let STXT = "STXT"
-EID = "EID"
 basic.pause(5000)
 radio.setGroup(82)
 radio.setTransmitPower(7)
 radio.setFrequencyBand(82)
 basic.turnRgbLedOff()
 basic.forever(function () {
+    basic.setLedColor(0x00ff00)
     Ti = convertToText(input.temperature())
     basic.pause(2000)
     Te = convertToText(DHT11.temperature())
@@ -28,6 +19,11 @@ basic.forever(function () {
     Le = convertToText(DHT11.humidity())
     basic.pause(2000)
     STXT = convertToText("" + SID + "," + Ti + "," + Te + "," + Le)
-    basic.showString(STXT)
+    basic.pause(2000)
     radio.sendString(STXT)
+    basic.pause(2000)
+    basic.showString(STXT)
+    basic.setLedColor(0xffff00)
+    basic.pause(60000)
+    basic.turnRgbLedOff()
 })
